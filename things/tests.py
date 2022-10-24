1dde60aa9f8764569167e3eb61367db23f31162d
muzami1ali <muzamilalibosan@icloud.com>
2022-10-24 21:28:55 +0100

    Add ThingForm

--- things/tests.py
+++ things/tests.py
@@ -1,3 +1,23 @@
+from django.core.exceptions import ValidationError
 from django.test import TestCase
+from .models import Thing
 
 # Create your tests here.
+class ThingModelTestCase(TestCase):
+
+    def setUp(self):
+        self.thing = Thing.objects.create_user(
+            name='@johndoe', 
+            description='Hi, my name is John.',
+            quantity =10
+        )
+
+    def test_assert_user_is_valid(self):
+        try:
+            self.user.full_clean()
+        except (ValidationError):
+            self.fail('Test user should be valid')
+
+    def test_assert_user_is_invalid(self):
+        with self.assertRaises(ValidationError):
+            self.user.full_clean()
